35dda934c713a258a27ad3c7ebe406a2d1401cb0
Thanh Dao <thanhdao@192.168.2.32>
2023-07-16 22:47:57 +0700

    add graphql config

---
+++ codegen.ts
@@ -0,0 +1,18 @@
+import { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: process.env.REACT_APP_API_URL,
+  documents: ['src/config/api/**/*.ts'],
+  generates: {
+    './src/__generated__/': {
+      preset: 'client',
+      plugins: [],
+      presetConfig: {
+        gqlTagName: 'gql',
+      }
+    }
+  },
+  ignoreNoDocuments: false,
+};
+
+export default config;
---
+++ src/config/graphql/photo.ts
@@ -0,0 +1,24 @@
+import { gql } from "../../__generated__";
+
+export const GET_PHOTOS = gql(`
+  query GET_PHOTOS($page: Int = 1, $limit: Int = 10, $search: String) {
+    photos(options: {
+      paginate: {
+        page: $page
+        limit: $limit
+      }
+      search: {
+        q: $search
+      }
+    }) {
+      data {
+        id
+        title
+        url
+      }
+      meta {
+        totalCount
+      }
+    }
+  }
+`)
